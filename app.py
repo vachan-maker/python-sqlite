1c8b545914b1d453d733d0cb2dad454f6ff7d68c
vachan-maker <vachan@tutanota.com>
2024-10-09 22:58:38 +0530

    Added database and created table

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import sqlite3
 conn = sqlite3.connect('employee.db')
+
+c = conn.cursor()
+
+# c.execute("""CREATE TABLE employees (
+#           first text,
+#           last text,
+#           pay integer)
+# """)
+
+c.execute("""
+    ALTER TABLE employees ADD pay integer
+""")
+conn.commit() #commits the current transaction to the database
+conn.close() #close the connection
